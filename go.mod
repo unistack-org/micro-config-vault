c625d497d45d8f97070239ce41ee43322a850dea
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-06-21 16:26:39 +0300

    update
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -1,9 +1,21 @@
 module github.com/unistack-org/micro-config-vault/v3
 
-go 1.15
+go 1.16
 
 require (
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.0 // indirect
 	github.com/hashicorp/vault/api v1.1.0
+	github.com/hashicorp/vault/sdk v0.2.0 // indirect
 	github.com/imdario/mergo v0.3.12
-	github.com/unistack-org/micro/v3 v3.3.17
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	github.com/unistack-org/micro/v3 v3.4.0
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- vault.go
+++ vault.go
@@ -101,7 +101,7 @@ func (c *vaultConfig) Init(opts ...config.Option) error {
 	return nil
 }
 
-func (c *vaultConfig) Load(ctx context.Context) error {
+func (c *vaultConfig) Load(ctx context.Context, opts ...config.LoadOption) error {
 	for _, fn := range c.opts.BeforeLoad {
 		if err := fn(ctx, c); err != nil && !c.opts.AllowFail {
 			return err
@@ -123,13 +123,22 @@ func (c *vaultConfig) Load(ctx context.Context) error {
 
 	if err == nil && pair != nil && pair.Data != nil {
 		var data []byte
+		var src interface{}
 		data, err = json.Marshal(pair.Data["data"])
 		if err == nil {
-			src, err := rutil.Zero(c.opts.Struct)
+			src, err = rutil.Zero(c.opts.Struct)
 			if err == nil {
 				err = c.opts.Codec.Unmarshal(data, src)
 				if err == nil {
-					err = mergo.Merge(c.opts.Struct, src, mergo.WithOverride, mergo.WithTypeCheck, mergo.WithAppendSlice)
+					options := config.NewLoadOptions(opts...)
+					mopts := []func(*mergo.Config){mergo.WithTypeCheck}
+					if options.Override {
+						mopts = append(mopts, mergo.WithOverride)
+					}
+					if options.Append {
+						mopts = append(mopts, mergo.WithAppendSlice)
+					}
+					err = mergo.Merge(c.opts.Struct, src, mopts...)
 				}
 			}
 		}
@@ -147,7 +156,7 @@ func (c *vaultConfig) Load(ctx context.Context) error {
 	return nil
 }
 
-func (c *vaultConfig) Save(ctx context.Context) error {
+func (c *vaultConfig) Save(ctx context.Context, opts ...config.SaveOption) error {
 	for _, fn := range c.opts.BeforeSave {
 		if err := fn(ctx, c); err != nil && !c.opts.AllowFail {
 			return err
